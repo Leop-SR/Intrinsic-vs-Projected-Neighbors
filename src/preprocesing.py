716ec41550ba84014b7ef4c2426469ef8d1ee710
Aslan <sanchez.ruiz.leonardo9@gmail.com>
2026-01-16 20:13:08 -0600

    PCA analysis using Robust Scaler and Standard Scaler

--- src/preprocesing.py
+++ src/preprocesing.py
@@ -2,3 +2,7 @@ import pandas as pd
 def robust_scaling (df: pd.DataFrame) -> pd.DataFrame:
     scaled_df = df.apply(lambda x: (x-x.median())/(x.quantile(0.75)-x.quantile(0.25)))
     return scaled_df
+
+def standarization (df: pd.DataFrame) -> pd.DataFrame:
+    scaled_df = df.apply(lambda x: ((x-x.mean())/x.std()))
+    return scaled_df
